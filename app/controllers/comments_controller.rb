c1f192c2b9c6902960a15b82491da2b5b49df8f8
Napatchol Thaipanich <patorseing@gmail.com>
2022-11-10 06:46:23 +0700

    feat: CRUD comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,9 @@
 class CommentsController < ApplicationController
+  def new
+    @article = Article.find(params[:id])
+    @comment = @article.comments.new
+  end
+
   def create
     @article = Article.find(params[:article_id])
     @comment = @article.comments.create(comment_params)
