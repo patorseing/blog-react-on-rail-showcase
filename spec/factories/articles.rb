ca01c87ea162fcb0981577129e3795a00fcf2dae
Napatchol Thaipanich <patorseing@gmail.com>
2022-11-20 10:06:02 +0700

    chore: use faker

--- spec/factories/articles.rb
+++ spec/factories/articles.rb
@@ -1,6 +1,8 @@
+require "faker"
+
 FactoryBot.define do
   factory :article do
-    title { 'Test test 1' }
-    body { 'Test test Body 1234' }
+    title { Faker::Lorem.sentence }
+    body { Faker::Lorem.characters(number: 10) }
   end
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -33,6 +33,11 @@ rescue ActiveRecord::PendingMigrationError => e
   abort e.to_s.strip
 end
 RSpec.configure do |config|
+  [:controller, :view, :request].each do |type|
+    config.include ::Rails::Controller::Testing::TestProcess, :type => type
+    config.include ::Rails::Controller::Testing::TemplateAssertions, :type => type
+    config.include ::Rails::Controller::Testing::Integration, :type => type
+  end
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
