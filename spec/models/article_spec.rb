c669c2f717ab558cb153908aa5fb8bc8368caa2c
Napatchol Thaipanich <patorseing@gmail.com>
2022-11-20 09:33:28 +0700

    chore: set up shoulda

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,10 +1,15 @@
-require 'rails_helper'
-
+require "rails_helper"
 
 RSpec.describe Article, type: :model do
   describe "valid factory" do
     subject { build(:article) }
 
     it { is_expected.to be_valid }
+  end
+
+  describe "validations" do
+    it { is_expected.to validate_presence_of :title }
+    it { is_expected.to validate_presence_of :body }
+    it { should validate_length_of(:body).is_at_least(10).on(:create) }
   end
 end
