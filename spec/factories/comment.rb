d40ce1a73e34f6f312b8196fded52736c89732d1
Napatchol Thaipanich <patorseing@gmail.com>
2022-11-20 10:20:38 +0700

    chore: test comment model

---
+++ spec/factories/comment.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require "faker"
+
+FactoryBot.define do
+  factory :comment do
+    commenter { Faker::Name.name }
+    body { Faker::Lorem.sentence }
+    article { build(:article) }
+  end
+end
---
+++ spec/models/comment_spec.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe Comment, type: :model do
+  describe "valid factory" do
+    subject { build(:comment) }
+
+    it { is_expected.to be_valid }
+    it { is_expected.to belong_to(:article) }
+  end
+end
