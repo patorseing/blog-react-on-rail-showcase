5fbf50489f1961c09e7226690a2ef30db76d2639
Napatchol Thaipanich <patorseing@gmail.com>
2022-11-26 13:29:53 +0700

    chore: update article spec

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -14,7 +14,7 @@ RSpec.describe Article, type: :model do
 
     it { is_expected.to validate_presence_of :title }
     it { is_expected.to validate_presence_of :body }
-    it { is_expected validate_length_of(:body).is_at_least(10).on(:create) }
+    it { is_expected validate_length_of(:body).is_at_least(10) }
   end
 
   describe 'associations' do
