246343ca7048dc57f8e8dce2190b3d0776310466
Napatchol Thaipanich <patorseing@gmail.com>
2022-11-28 08:26:04 +0700

    chore: fix text model

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -12,12 +12,12 @@ RSpec.describe Article, type: :model do
   describe 'validations' do
     before { create(:article) }
 
-    it { is_expected.to validate_presence_of :title }
-    it { is_expected.to validate_presence_of :body }
-    it { is_expected validate_length_of(:body).is_at_least(10) }
+    it { is_expected.to validate_presence_of(:title) }
+    it { is_expected.to validate_presence_of(:body) }
+    it { is_expected.to validate_length_of(:body).is_at_least(10) }
   end
 
   describe 'associations' do
-    it { is_expected have_many(:comments).dependent(:destroy) }
+    it { is_expected.to have_many(:comments).dependent(:destroy) }
   end
 end
